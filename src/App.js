fbd8d7dbc34fa90b2f0c7886fd45216d1abe0388
LidiaStrelets <0630610171li@gmail.com>
2021-06-29 21:27:16 +0300

    error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React from "react";
+// import React from "react";
 import Gallery from "./components/PaintingsGallery";
 import paintingsData from "./paintings.json";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>My app</h1>
-      
+
         <Gallery paintings={paintingsData} />
       </header>
     </div>
